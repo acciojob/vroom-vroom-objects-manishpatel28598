b9f7028be4e652ccadcfa1ea8c6b30b23abcf47e
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-21 19:23:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,14 @@ function SportsCar(make, model, topSpeed) {
 	this.topSpeed = topSpeed;
 }
 SportsCar.prototype.getTopSpeed = function(){
-return `${this.topSpeed}`;
+return this.topSpeed;
 }
 
 
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel());
 console.log(car.getTopSpeed());
+
 
 // Do not change the code below
 window.Car = Car;
